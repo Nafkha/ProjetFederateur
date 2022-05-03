d20d73766ae2250b2c62bdb8c6261cfd32c3d3ed
Mohamed Youssef Nafkha <46995112+Nafkha@users.noreply.github.com>
2022-05-03 21:58:23 +0100

    Authentification Front/Back

--- src/main/java/com/example/project/App.java
+++ src/main/java/com/example/project/App.java
@@ -5,13 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+
+import java.sql.*;
+
 
 public class App extends Application
 {
+    static String db = "jdbc:mysql://localhost:3306/projetjava";
+    static String username = "root";
+    static String password = "admin";
+    static Connection con;
     @Override
     public void start(Stage stage) throws IOException
     {
+        try{
+            con = DriverManager.getConnection(db,username,password);
+            System.out.println("Database connected");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 674, 429);
         stage.setTitle("App");
--- src/main/java/com/example/project/LoginController.java
+++ src/main/java/com/example/project/LoginController.java
@@ -8,10 +8,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable
@@ -19,19 +23,44 @@ public class LoginController implements Initializable
     private Stage stage;
     private Scene scene;
     private Parent root;
+    static String admin;
+    private String pw;
 
     @FXML
     private Button loginbtn;
+    @FXML
+    private TextField user;
+    @FXML
+    private TextField pass;
 
     @FXML
     void login_click(ActionEvent event) throws IOException
     {
-        root = FXMLLoader.load(getClass().getResource("Home.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setMaximized(true);
-        stage.show();
+        admin = user.getText();
+        pw = pass.getText();
+        try{
+            PreparedStatement pstmt = App.con.prepareStatement("Select * from utilisateur where(Login = ? AND Password = ?)");
+            pstmt.setString(1,admin);
+            pstmt.setString(2,pw);
+            ResultSet rs =   pstmt.executeQuery();
+            if(rs.next()) {
+
+                root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setMaximized(true);
+                stage.show();
+            }else{
+                System.out.println("utilisateur n'existe pas");
+            }
+
+        }catch(SQLException e){
+            System.out.println("User does not exist");
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
